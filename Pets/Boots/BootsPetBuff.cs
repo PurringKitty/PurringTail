a27e197409569eb428402d1b22169d0ed20f5070
Purring Kitty <146129451+PurringKitty@users.noreply.github.com>
2023-09-26 12:53:37 -0700

    Add files via upload

---
+++ Pets/Boots/BootsPetBuff.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using PurringTail.Content.Pets.Boots;
+
+namespace PurringTail.Content.Pets.Boots
+{
+	public class BootsPetBuff : ModBuff
+	{
+		public override void SetStaticDefaults() {
+			Main.buffNoTimeDisplay[Type] = true;
+			Main.vanityPet[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex) { // This method gets called every frame your buff is active on your player.
+			bool unused = false;
+			player.BuffHandle_SpawnPetIfNeededAndSetTime(buffIndex, ref unused, ModContent.ProjectileType<BootsPetProjectile>());
+		}
+	}
+}

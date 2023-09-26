190a0761cd1e253b59d3d5010fe3c53db3e1f85b
Purring Kitty <146129451+PurringKitty@users.noreply.github.com>
2023-09-26 12:48:04 -0700

    Add files via upload

---
+++ Buffs/GodSlayerMinionBuff.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using PurringTail.Content;
+using PurringTail.Content.Items.Weapons;
+using PurringTail.Content.Projectiles;
+
+namespace PurringTail.Content.Buffs
+{
+    public class GodSlayerMinionBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<GodSlayerMinionProjectile>()] > 0)
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+            else
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+        }
+    }
+}
